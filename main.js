8a301f4055523fda0f547930b267527573c477b9
Ata Gülalan <ata@xava.me>
2021-01-30 12:30:58 +0300

    Run on Windows

--- main.js
+++ main.js
@@ -3,52 +3,22 @@ const {
 // eslint-disable-next-line import/no-extraneous-dependencies
 } = require('electron');
 const path = require('path');
+const trayWindow = require('electron-tray-window');
 
 const assetsDirectory = path.join(__dirname, 'assets');
 
 let tray;
 let window;
 
-const getWindowPosition = () => {
-  const windowBounds = window.getBounds();
-  const trayBounds = tray.getBounds();
-
-  // Center window horizontally below the tray icon
-  const x = Math.round(trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2);
-
-  // Position window 4 pixels vertically below the tray icon
-  const y = Math.round(trayBounds.y + trayBounds.height + 4);
-
-  return { x, y };
-};
-
-const showWindow = () => {
-  const position = getWindowPosition();
-  window.setPosition(position.x, position.y, false);
-  window.show();
-  window.focus();
-};
-
-const toggleWindow = () => {
-  if (window.isVisible()) {
-    window.hide();
-  } else {
-    showWindow();
-  }
-};
-
 const createTray = () => {
   tray = new Tray(path.join(assetsDirectory, 'headphones.png'));
-  tray.on('right-click', toggleWindow);
-  tray.on('double-click', toggleWindow);
   tray.on('click', (event) => {
-    toggleWindow();
-
     // Show devtools when command clicked
     if (window.isVisible() && process.defaultApp && event.metaKey) {
       window.openDevTools({ mode: 'detach' });
     }
   });
+  return tray;
 };
 
 const createWindow = () => {
@@ -73,21 +43,19 @@ const createWindow = () => {
     },
   });
   window.loadURL(`file://${path.join(__dirname, 'index.html')}`);
-
-  // Hide the window when it loses focus
-  window.on('blur', () => {
-    if (!window.webContents.isDevToolsOpened()) {
-      window.hide();
-    }
-  });
+  return window;
 };
 
 // Don't show the app in the doc
-app.dock.hide();
+if (app.dock && app.dock.hide) {
+  app.dock.hide();
+}
 
 app.on('ready', () => {
-  createTray();
-  createWindow();
+  trayWindow.setOptions({
+    tray: createTray(),
+    window: createWindow(),
+  });
 });
 
 // Quit the app when the window is closed
