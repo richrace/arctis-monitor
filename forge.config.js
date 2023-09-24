6a8091aff95fc7db630955d34028ac6970013987
Richard Race <1273965+richrace@users.noreply.github.com>
2023-09-24 12:27:35 +0100

    Tweak icons

--- forge.config.js
+++ forge.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   packagerConfig: {
-    icon: './assets/icon.icns',
+    icon: './assets/icon',
     out: './out',
     overwrite: true,
     executableName: 'arctis-monitor',
@@ -12,6 +12,7 @@ module.exports = {
       config: {
         name: 'arctis-monitor',
         iconUrl: 'https://raw.githubusercontent.com/richrace/arctis-monitor/main/assets/icon.ico',
+        setupIcon: './assets/icon.ico'
       },
     },
     {
@@ -22,6 +23,7 @@ module.exports = {
       name: '@electron-forge/maker-dmg',
       config: {
         format: 'ULFO',
+        icon: './assets/icon.icns'
       },
     },
     {
